bf67ae896ff66d1c212b39a5141e90cf0fa0c2ac
Fatemeh Noorali <F.Noorali@Vesalgasht.local>
2023-10-22 09:28:11 +0330

    get method, post method with form

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 const express = require("express");
-const { products } = require("./data");
 const app = express();
+const { people } = require("./data");
 
-// req => middleware => res
+//static assets
+app.use(express.static('./public'))
 
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  res.send("testing");
-  // next() -> go to next function
-};
+//parse form data
+app.use(express.urlencoded({extended:false}))
 
-app.get("/", logger, (req, res) => {
-  res.send("home");
+// get method
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
-app.use("/api", logger); //apply  it to the rest of the routes, all of them!
 
-app.get("/about", (req, res) => {
-  res.send("about");
-});
-app.all("*", (req, res) => {
-  res.status(404).send("resourse not found");
-});
+app.post('/login',(req,res)=>{
+  // console.log(req.body) // get form data with app.use-urlencoded
+  const {name} = req.body
+  if (name) {
+    return res.status(200).send(`welcome ${name}`)
+  }
+   res.status(401 ).send('please enter name')
+})
 
 app.listen(5000, (req, res) => {
   console.log("server is listening on 5000");
